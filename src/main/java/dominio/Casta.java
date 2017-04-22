73c65a90105b22b1755c0b19d50cc48fcf5ef432
sntejada1 <sntejada1@gmail.com>
2017-04-22 02:07:00 -0300

    Add files via upload

--- src/main/java/dominio/Casta.java
+++ src/main/java/dominio/Casta.java
@@ -1,7 +1,11 @@
 package dominio;
 
 import java.io.Serializable;
-
+/**
+ * 
+ * Establece las habilidades basicas disponibles para todas las diferentes clases (o castas) de jugadores
+ *
+ */
 public abstract class Casta implements Serializable {
 	protected double probabilidadGolpeCritico;
 	protected double probabilidadEvitarDaño;
@@ -10,18 +14,32 @@ public abstract class Casta implements Serializable {
 
 	protected String[] habilidadesCasta;
 
+	/**
+	 *  Establece el valor por defecto de las diferentes habilidades de la casta.
+	 */
 	public Casta() {
 		this.probabilidadGolpeCritico = 0.2;
 		this.probabilidadEvitarDaño = 0.2;
 		this.dañoCritico = 1.5;
 	}
 
+	/**
+	 * 
+	 * 	Establece las estadisticas para las diferentes habilidades de una casta determinada
+	 * 
+	 * @param prob_crit
+	 * @param evasion
+	 * @param daño_crit
+	 */
 	public Casta(double prob_crit, double evasion, double daño_crit) {
 		this.probabilidadGolpeCritico = prob_crit;
 		this.probabilidadEvitarDaño = evasion;
 		this.dañoCritico = daño_crit;
 	}
 
+	/**
+	 * Cada personaje tiene disponible 3 habilidades, siendo estas diferentes y haciendo distinto tipos de da�o.
+	 */
 	public abstract boolean habilidad1(Personaje caster, Peleable atacado);
 
 	public abstract boolean habilidad2(Personaje caster, Peleable atacado);
@@ -40,6 +58,11 @@ public abstract class Casta implements Serializable {
 		return probabilidadGolpeCritico;
 	}
 
+	/**
+	 * establece un valor determinado para la habilidad de la casta "probabilidadGolpeCritico"
+	 * 
+	 * @param probabilidadGolpeCritico
+	 */
 	public void setProbabilidadGolpeCritico(double probabilidadGolpeCritico) {
 		this.probabilidadGolpeCritico = probabilidadGolpeCritico;
 	}
