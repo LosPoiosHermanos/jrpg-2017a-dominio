50c41ab041bf1901b6e12259052bc45a76786098
Marcelo Gabriel Lorenz <marcelo.lorenz@hotmail.com>
2017-05-04 15:24:58 -0300

    Add files via upload

--- src/main/java/dominio/Alianza.java
+++ src/main/java/dominio/Alianza.java
@@ -1,8 +1,11 @@
+/*
+ * 
+ */
 package dominio;
 
 import java.util.LinkedList;
 
-
+// TODO: Auto-generated Javadoc
 /**
  * Añade o elimina jugadores a un equipo determinado
  * 
@@ -15,22 +18,35 @@ public class Alianza {
 	LinkedList<Personaje> aliados;
 
 
+	/**
+	 * crea una alianza con un nombre determinado
+	 * @param nombre
+	 */
 	public Alianza(String nombre) {
 		this.nombre = nombre;
 		this.aliados = new LinkedList <Personaje>();
 	}
 
-
+	/**
+	 * devuelve todos los miembros de la alianza
+	 * @return
+	 */
 	public LinkedList<Personaje> getAliados() {
 		return aliados;
 	}
 
-
+	/**
+	 * Agrega una lista de miembros a la alianza
+	 * @param aliados
+	 */
 	public void setAliados(LinkedList<Personaje> aliados) {
 		this.aliados = aliados;
 	}
 
-
+	/**
+	 * devuelve el nombre de la alianza
+	 * @return
+	 */
 	public String obtenerNombre(){
 		return nombre;
 	}
