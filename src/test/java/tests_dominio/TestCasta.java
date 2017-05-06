37cd632e571a1b12da80a8c80e3551efc7610dac
HernanEnciso <enciso.hernan@yahoo.com.ar>
2017-05-06 03:55:28 -0300

    Add files via upload

--- src/test/java/tests_dominio/TestCasta.java
+++ src/test/java/tests_dominio/TestCasta.java
@@ -1,31 +1,26 @@
 package tests_dominio;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import dominio.Asesino;
 import dominio.Casta;
 import dominio.MyRandom;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestCasta {
-	@Test
-	public void testHabilidades(){
-		Casta c1 = new Asesino();
-		MyRandom r = new MyRandom();
-		Assert.assertNotNull(c1.getHabilidadesCasta());
-		Assert.assertNotNull(c1.getNombreCasta());
-		
-		double danio = c1.getDañoCritico();
-		c1.setDañoCritico(33.33);
-		Assert.assertTrue(danio != c1.getDañoCritico());
-		
-		danio = c1.getProbabilidadEvitarDaño();
-		c1.setProbabilidadEvitarDaño(33.33);
-		Assert.assertTrue(danio != c1.getProbabilidadEvitarDaño());
-		
-		danio = c1.getProbabilidadGolpeCritico();
-		c1.setProbabilidadGolpeCritico(33.33);
-		Assert.assertTrue(danio != c1.getProbabilidadGolpeCritico());
-		
-	}
+  @Test
+  public void testHabilidades() {
+    Casta c1 = new Asesino();
+    MyRandom r = new MyRandom();
+    Assert.assertNotNull(c1.getHabilidadesCasta());
+    Assert.assertNotNull(c1.getNombreCasta());
+    double danio = c1.getDañoCritico();
+    c1.setDañoCritico(33.33);
+    Assert.assertTrue(danio != c1.getDañoCritico());
+    danio = c1.getProbabilidadEvitarDaño();
+    c1.setProbabilidadEvitarDaño(33.33);
+    Assert.assertTrue(danio != c1.getProbabilidadEvitarDaño());
+    danio = c1.getProbabilidadGolpeCritico();
+    c1.setProbabilidadGolpeCritico(33.33);
+    Assert.assertTrue(danio != c1.getProbabilidadGolpeCritico());
+  }
 }
--- src/test/java/tests_dominio/TestHumano.java
+++ src/test/java/tests_dominio/TestHumano.java
@@ -1,44 +1,39 @@
 package tests_dominio;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import dominio.Asesino;
 import dominio.Elfo;
 import dominio.Hechicero;
 import dominio.Humano;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestHumano {
 
-	@Test
-	public void testIncentivar() {
-		Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
-		Elfo e = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 3, 1);
+  @Test
+  public void testIncentivar() {
+    Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
+    Elfo e = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 3, 1);
+    Assert.assertTrue(e.getAtaque() == 37);
+    h.habilidadRaza1(e);
+    Assert.assertTrue(e.getAtaque() > 37);
+  }
 
-		Assert.assertTrue(e.getAtaque() == 37);
-		h.habilidadRaza1(e);
-		Assert.assertTrue(e.getAtaque() > 37);
-	}
-
-	@Test
-	public void testGolpeFatal() {
-		Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
-		Elfo e = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 3, 1);
-		Elfo e2 = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 100, 1);
-		Humano h2 = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
-		h2.setEnergia(5);
-		
-		Assert.assertTrue(h.getEnergia() == 100);
-		Assert.assertTrue(e.getSalud() == 100);
-		if (h.habilidadRaza2(e)) {
-			Assert.assertTrue(e.getSalud() == 70);
-			Assert.assertTrue(h.getEnergia() == 50);
-		}
-		if (!h2.habilidadRaza2(e2)) {
-			Assert.assertFalse(e2.getSalud() == 70);
-			Assert.assertFalse(h2.getEnergia() == 50);
-		}
-		
-
-	}
-	}
+  @Test
+  public void testGolpeFatal() {
+    Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
+    Elfo e = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 3, 1);
+    Humano h2 = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
+    h2.setEnergia(5);
+    Assert.assertTrue(h.getEnergia() == 100);
+    Assert.assertTrue(e.getSalud() == 100);
+    if (h.habilidadRaza2(e)) {
+      Assert.assertTrue(e.getSalud() == 70);
+      Assert.assertTrue(h.getEnergia() == 50);
+    }
+    Elfo e2 = new Elfo("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 100, 1);
+    if (!h2.habilidadRaza2(e2)) {
+      Assert.assertFalse(e2.getSalud() == 70);
+      Assert.assertFalse(h2.getEnergia() == 50);
+    }
+  }
+}
