1c1a37f95e18210f889d7dea9e4ed247212341dc
sntejada1 <sntejada1@gmail.com>
2017-06-14 19:32:15 -0300

    Se vacian los test ya que cambian las clases

--- src/test/java/tests_dominio/TestObjeto.java
+++ src/test/java/tests_dominio/TestObjeto.java
@@ -6,9 +6,7 @@ import java.util.HashMap;
 
 import org.junit.Test;
 
-import dominio.Arma;
-import dominio.Armadura;
-import dominio.Guantes;
+
 import dominio.Guerrero;
 import dominio.Humano;
 import dominio.MyRandomStub;
@@ -17,59 +15,5 @@ import dominio.Orco;
 
 public class TestObjeto {
 
-	@Test
-	public void testAtributoRandom() {
-		
-		Objeto o1 = new Armadura();
-		Objeto o2 = new Guantes();
-		Objeto o3 = new Arma();
-		
-		System.out.println(o1.getAtributo());
-		System.out.println(o2.getAtributo());
-		System.out.println(o3.getAtributo());
-		
-	}
-	
-	@Test
-	public void testObjetoAlGanar(){
-		
-		Humano h = new Humano("Nico", 100, 100, 100, 20, 30, new Guerrero(0.2, 0.3, 1.5), 0, 1, 1);
-		Orco o = new Orco("Nico", 100, 100, 15, 0, 30, new Guerrero(0.2, 0, 1.5), 0, 1, 1);
-		
-		HashMap <String,Integer> mapa = new HashMap<String,Integer>();
-		mapa.put("salud", 1);
-		o.actualizar(mapa);
 
-		System.out.println("inventario antes: ");
-			h.VerInventario();
-			
-			h.atacar(o, new MyRandomStub(0));
-		
-		System.out.println("inventario despues: ");
-			h.VerInventario();
-			
-	}
-	
-	@Test
-	public void testAtributosObjeto(){
-		
-		Humano h = new Humano("Nico", 100, 100, 100, 20, 30, new Guerrero(0.2, 0.3, 1.5), 0, 1, 1);
-		
-		System.out.println("antes:");
-		System.out.println("fuerza: "+h.getFuerza() + " defensa: "+ h.getDefensa()+ " salud tope: "+ h.getSaludTope());
-		
-		h.ganarObjeto();
-		
-		System.out.println("inventario: ");
-		System.out.println(" ");
-		h.VerInventario();
-			
-			System.out.println("despues:");
-			System.out.println("fuerza: "+h.getFuerza() + " defensa: "+ h.getDefensa()+ " salud tope: "+ h.getSaludTope());		
-			
-	}
-	
-	
-	
-	
 }
