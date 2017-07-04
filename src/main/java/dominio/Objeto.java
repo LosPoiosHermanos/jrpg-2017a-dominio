082edb8d6f2dbed9e578b01e0cecef102c3c3177
HernanEnciso <enciso.hernan@yahoo.com.ar>
2017-07-04 20:38:36 -0300

    Cambio en la matriz de objetos
    son los objetos posta posta

--- src/main/java/dominio/Objeto.java
+++ src/main/java/dominio/Objeto.java
@@ -3,12 +3,11 @@ package dominio;
 public class Objeto {
 
 	protected Integer id;
-	
+
 	protected String nombre;
 	protected int atributo;
 	protected String atributoModificado;
 	protected MyRandom rand = new MyRandom();
-	
 
 	public Objeto() {
 		id = rand.nextInt(19) + 1;
@@ -23,27 +22,24 @@ public class Objeto {
 		id = key;
 		asignarObjeto();
 	}
-/**
- * Los objetos dan bonus para los atributos : fuerza saludtope y destreza
- */
+
+	/**
+	 * Los objetos dan bonus para los atributos : saludTope, energiaTope, destreza, defensa, inteligencia y fuerza
+	 */
 	private void asignarObjeto() {
-		String matrizObjetos[][] = {{"Brazalete","10","fuerza"},{"Cuchilla","10","fuerza"},
-				{"Guantes","10","fuerza"},{"Botas","10","fuerza"},
-				{"Martillo","10","fuerza"},{"Espada","10","fuerza"},
-				{"Lanza","10","fuerza"},{"Martillo","10","fuerza"},
-				{"Hacha","10","fuerza"},{"Armadura","10","fuerza"},
-				{"Boomerang","10","fuerza"},{"Anillo","10","fuerza"},
-				{"Capa","10","fuerza"},{"Sombrero","10","fuerza"},
-				{"Casco","10","fuerza"},{"Escudo","10","fuerza"},
-				{"Katana","10","fuerza"},{"Cetro","10","fuerza"},
-				{"Posion","10","fuerza"},{"Espada","10","fuerza"}};
-		
-		if( id > 0 && id < matrizObjetos.length ){
-			nombre = matrizObjetos[id-1][0] ;
-			atributo = Integer.parseInt(matrizObjetos[id-1][1]); 
-			atributoModificado = matrizObjetos[id-1][2];
+		String matrizObjetos[][] = { { "Brazalete", "10", "saludTope" }, { "Collar", "25", "saludTope" },
+				{ "Amuleto", "50", "saludTope" }, { "Botas", "10", "energiaTope" }, { "Anillo", "25", "energiaTope" },
+				{ "Pendientes", "50", "energiaTope" }, { "Guantes", "5", "destreza" }, { "Shuriken", "15", "destreza" },
+				{ "Katana", "25", "destreza" }, { "Casco", "10", "defensa" }, { "Escudo", "15", "defensa" },
+				{ "Armadura", "20", "defensa" }, { "Capa", "10", "inteligencia" }, { "Sombrero", "20", "inteligencia" },
+				{ "Cetro", "30", "inteligencia" }, { "Cuchillo", "5", "fuerza" }, { "Martillo", "10", "fuerza" },
+				{ "Lanza", "15", "fuerza" }, { "Hacha", "20", "fuerza" }, { "Espada", "20", "fuerza" } };
+
+		if (id > 0 && id < matrizObjetos.length) {
+			nombre = matrizObjetos[id - 1][0];
+			atributo = Integer.parseInt(matrizObjetos[id - 1][1]);
+			atributoModificado = matrizObjetos[id - 1][2];
 		}
-		
 
 	}
 
